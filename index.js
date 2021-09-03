007c5c7f28475f5a18e687d2baa92ef285b496dc
Glitchy <70556971+trollar@users.noreply.github.com>
2021-09-03 19:49:50 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const noblox = require('noblox.js')
+var prompt = require('prompt-sync')();
+var fs = require("fs")
+const { setTimeout } = require('timers/promises');
+
+async function main() {
+
+var Name = prompt('Please enter a asset name ');
+var filepath = prompt('Please enter the filepath ')
+var TimeInMilliseconds = prompt('Please enter a time [in miliseconds] ');
+var Cookie = prompt('Please enter your cookie ');
+var CatalogId = prompt('Please enter the asset type number ') // get them at https://developer.roblox.com/en-us/api-reference/enum/AssetType
+console.log("file scheduled to upload")
+const currentUser = await noblox.setCookie(Cookie) 
+await setTimeout(TimeInMilliseconds)
+console.log("uploaded the asset")
+console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
+noblox.uploadItem(`${Name}`, CatalogId, fs.createReadStream(filepath))
+}
+
+main()
